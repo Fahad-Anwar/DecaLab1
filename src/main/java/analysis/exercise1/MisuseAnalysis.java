9ae860a860ef39ab704a61d2aab93f79eb3d6468
Fahad-Anwar <sjdff1@gmail.com>
2018-11-12 02:11:43 +0100

    final solution of excercise 1 of 2

--- src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -3,9 +3,8 @@ package analysis.exercise1;
 import analysis.AbstractAnalysis;
 import analysis.VulnerabilityReporter;
 import soot.Body;
-import soot.SootClass;
-import soot.SootMethodRef;
 import soot.Unit;
+import soot.SootMethodRef;
 import soot.Value;
 import soot.jimple.InvokeExpr;
 import soot.jimple.StaticInvokeExpr;
@@ -23,49 +22,41 @@ public class MisuseAnalysis extends AbstractAnalysis{
 		if (unit instanceof JAssignStmt) {
 			Stmt stmt = (Stmt) unit;
 			if (stmt.containsInvokeExpr()) {
-				InvokeExpr exp = ((JAssignStmt) unit).getInvokeExpr();
-				
-				if (exp instanceof StaticInvokeExpr) {
-					SootMethodRef method = exp.getMethodRef();
-					if (method.isStatic() && method.getSubSignature().toString()
+				InvokeExpr invokeExpr = ((JAssignStmt) unit).getInvokeExpr();
+				if (invokeExpr instanceof StaticInvokeExpr) {
+					SootMethodRef sootMethodRef = invokeExpr.getMethodRef();
+					if (sootMethodRef.isStatic() && sootMethodRef.getSubSignature().toString()
 							.equals("javax.crypto.Cipher getInstance(java.lang.String)")) {
-						/*
-						 * Question: is it better to use this method?
-						 */
-						
-						
-//						if (exp.getArgCount() == 1) {
-//							Value arg = exp.getArg(0);
-//							if (arg instanceof StringConstant) {
-//								if (((StringConstant) arg).value.equals("AES")) {
-//									if (!arg.toString().equals("\"AES/GCM/PKCS5Padding\"")) {
-//										this.reporter.reportVulnerability(method.getSignature(), unit);
-//									}
-//								}
-//							}
-//						}
-					}
-					if (method.isStatic()) {
-						if (method.name().equals("getInstance")) {
-							SootClass decClass = method.declaringClass();
-							if (decClass.getName().equals("javax.crypto.Cipher")
-									&& method.returnType().toString().equals("javax.crypto.Cipher")) {
-								if (exp.getArgCount() == 1) {
-									Value arg = exp.getArg(0);
-									if (arg instanceof StringConstant) {
-										if (((StringConstant) arg).value.equals("AES")) {
-											if (!arg.toString().equals("\"AES/GCM/PKCS5Padding\"")) {
-												this.reporter.reportVulnerability(method.getSignature(), unit);
-											}
-										}
-									}
+						if (invokeExpr.getArgCount() == 1) {
+							Value arg = invokeExpr.getArg(0);
+							if (arg instanceof StringConstant) {
+								if (((StringConstant) arg).value.equals("AES")
+										|| !((StringConstant) arg).value.equals("AES/GCM/PKCS5Padding")) {
+									this.reporter.reportVulnerability(this.method.toString(), unit);
 								}
 							}
 						}
 					}
+					/*
+					 * Question: is it better to use this soloution instead of above?
+					 */
+//					if (method.isStatic() && method.name().equals("getInstance")) {
+//						SootClass decClass = method.declaringClass();
+//						if (decClass.getName().equals("javax.crypto.Cipher")
+//								&& method.returnType().toString().equals("javax.crypto.Cipher")) {
+//							if (exp.getArgCount() == 1) {
+//								Value arg = exp.getArg(0);
+//								if (arg instanceof StringConstant) {
+//									if (((StringConstant) arg).value.equals("AES")
+//											|| !((StringConstant) arg).value.equals("AES/GCM/PKCS5Padding")) {
+//										this.reporter.reportVulnerability(this.method.toString(), unit);
+//									}
+//								}
+//							}
+//						}
+//					}
 				}
 			}
-
 		}
 	}
 }
