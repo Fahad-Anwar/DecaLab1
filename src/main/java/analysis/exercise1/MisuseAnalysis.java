fd27c139a9b452743154ad6b6efa7bc6f5768600
Fahad-Anwar <sjdff1@gmail.com>
2018-11-12 01:47:03 +0100

    solved excercise 1 of 2

--- src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -3,7 +3,15 @@ package analysis.exercise1;
 import analysis.AbstractAnalysis;
 import analysis.VulnerabilityReporter;
 import soot.Body;
+import soot.SootClass;
+import soot.SootMethodRef;
 import soot.Unit;
+import soot.Value;
+import soot.jimple.InvokeExpr;
+import soot.jimple.StaticInvokeExpr;
+import soot.jimple.Stmt;
+import soot.jimple.StringConstant;
+import soot.jimple.internal.JAssignStmt;
 
 public class MisuseAnalysis extends AbstractAnalysis{
 	public MisuseAnalysis(Body body, VulnerabilityReporter reporter) {
@@ -12,6 +20,52 @@ public class MisuseAnalysis extends AbstractAnalysis{
 	
 	@Override
 	protected void flowThrough(Unit unit) {
-		// TODO: Implement your analysis here.
+		if (unit instanceof JAssignStmt) {
+			Stmt stmt = (Stmt) unit;
+			if (stmt.containsInvokeExpr()) {
+				InvokeExpr exp = ((JAssignStmt) unit).getInvokeExpr();
+				
+				if (exp instanceof StaticInvokeExpr) {
+					SootMethodRef method = exp.getMethodRef();
+					if (method.isStatic() && method.getSubSignature().toString()
+							.equals("javax.crypto.Cipher getInstance(java.lang.String)")) {
+						/*
+						 * Question: is it better to use this method?
+						 */
+						
+						
+//						if (exp.getArgCount() == 1) {
+//							Value arg = exp.getArg(0);
+//							if (arg instanceof StringConstant) {
+//								if (((StringConstant) arg).value.equals("AES")) {
+//									if (!arg.toString().equals("\"AES/GCM/PKCS5Padding\"")) {
+//										this.reporter.reportVulnerability(method.getSignature(), unit);
+//									}
+//								}
+//							}
+//						}
+					}
+					if (method.isStatic()) {
+						if (method.name().equals("getInstance")) {
+							SootClass decClass = method.declaringClass();
+							if (decClass.getName().equals("javax.crypto.Cipher")
+									&& method.returnType().toString().equals("javax.crypto.Cipher")) {
+								if (exp.getArgCount() == 1) {
+									Value arg = exp.getArg(0);
+									if (arg instanceof StringConstant) {
+										if (((StringConstant) arg).value.equals("AES")) {
+											if (!arg.toString().equals("\"AES/GCM/PKCS5Padding\"")) {
+												this.reporter.reportVulnerability(method.getSignature(), unit);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+		}
 	}
 }
